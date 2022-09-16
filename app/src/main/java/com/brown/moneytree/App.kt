f7abed3a08d73e4e96c014ed58929c7e856187d9
tnlanh99 <tnlanh99@gmail.com>
2022-09-17 00:18:29 +0700

    Add logger module to app

--- app/src/main/java/com/brown/moneytree/App.kt
+++ app/src/main/java/com/brown/moneytree/App.kt
@@ -1,7 +1,18 @@
 package com.brown.moneytree
 
 import android.app.Application
+import com.brown.moneytree.core.logger.Logger
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    @Inject
+    internal lateinit var logger: Logger
+
+    override fun onCreate() {
+        super.onCreate()
+        logger.setup(BuildConfig.DEBUG)
+    }
+}
